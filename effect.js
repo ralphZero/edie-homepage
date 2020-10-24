5c8d63893cb57b6458d43d961f8d3257073d89a9
ralphZero <placideralph@gmail.com>
2020-10-24 00:52:15 -0400

    fixed scrollspy issue

--- effect.js
+++ effect.js
@@ -1,24 +1,39 @@
 function isInViewport(element){
     const rect = element.getBoundingClientRect();
+    const middle = (rect.bottom + rect.top) / 2;
     return (
         rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        middle <= (window.innerHeight || document.documentElement.clientHeight)
     );
 }
-const deck = document.querySelector('.services-deck');
-let card = document.querySelectorAll('.services-card');
+
 (function(){
+    const deck = document.querySelector('.services-deck');
+    let card = document.querySelectorAll('.services-card');
+    let design_cards = document.querySelectorAll('.card');
+
     document.addEventListener('scroll', (e) => {
         if(isInViewport(deck)){
-            for(let i=0; i < card.length; i++){
+            for(let i=0, c=card.length; i < c; i++){
                 card[i].classList.add('services-card--animation-'+i);
             }
         }
     }, false);
 
+    let test = document.querySelector('#test');
+    document.addEventListener('scroll',(e)=>{
+        for(let i=0, d=design_cards.length;  i < d; i++){
+            if(isInViewport(design_cards[i])){
+                design_cards[i].classList.add('card--animation-'+i);
+            }
+        }
+    }, false);
+
     window.onload = (e)=>{
+
+        console.log('Viewport height: '+window.innerHeight);
+        console.log('Viewport width: '+window.innerWidth);
+
         let ourwork = document.querySelectorAll('.ourwork');
         for(let i=0; i < ourwork.length; i++){
             ourwork[i].classList.add('ourwork--animation-'+i);
@@ -26,6 +41,10 @@ let card = document.querySelectorAll('.services-card');
         
         for(let i=0; i < card.length; i++){
             card[i].classList.add('services-card--opacity-0');
+        }
+
+        for(let i=0; i <  design_cards.length; i++){
+            design_cards[i].classList.add('card-opacity');
         }
     };
 })();
